6384d128158ae85daa16a2555fcfbc17226a588e
wendersoon <ds.wendersonmelo@gmail.com>
2025-08-08 08:28:00 -0300

    1.9

---
+++ chapter_1/exercise_1_9.py
@@ -0,0 +1,36 @@
+"""
+I
+(define (+ a b)
+    (if (= a 0) 
+        b 
+        (inc (+ (dec a) b))))
+
+II
+(define (+ a b)
+    (if (= a 0) 
+        b 
+        (+ (dec a) (inc b))))
+
+
+-----------------------------
+I - Recursive process
+(+ 4 5)
+(inc(+ 3 5))
+(inc(inc(+ 2 5)))
+(inc(inc(inc(+ 1 5))))
+(inc(inc(inc(inc(+ 0 5)))))
+(inc(inc(inc(6))))
+(inc(inc(7)))
+(inc(8))
+9
+
+II - Iterative process
+(+ 4 5)
+(+ 3 6)
+(+ 2 7)
+(+ 1 8)
+(+ 0 9)
+9
+
+
+"""
