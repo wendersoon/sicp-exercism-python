3da5815f244a2052cee735037aadc0d26bd8319a
wendersoon <ds.wendersonmelo@gmail.com>
2025-08-12 07:30:11 -0300

    1.11

--- chapter_1/exercise_1_11.py
+++ chapter_1/exercise_1_11.py
@@ -9,21 +9,16 @@ print(f(5))
 print(f(7))
 print(5*'-')
 # Generates Iterative Process
-def f_loop(n, n1, n2, n3, nth):
-    if n == nth:
-        return n1
-    else:
-        return f_loop(n, (n1 + 2 * n2 + 3 * n3),
-                        n1, 
-                        n2, 
-                        1 + nth
-                        )
-        
-def f_iterative(n):
+def f_iter_loop(n):
     if n < 3:
         return n
-    else:
-        return f_loop(n, 2, 1, 0, 2)
+    n3, n2, n1 = 0, 1, 2  # f(0), f(1), f(2)
+    nth = 2
+    while nth < n:
+        novo = n1 + 2*n2 + 3*n3
+        n3, n2, n1 = n2, n1, novo
+        nth += 1
+    return n1
     
-print(f_iterative(5))
-print(f_iterative(7))
+print(f_iter_loop(5))
+print(f_iter_loop(7))
