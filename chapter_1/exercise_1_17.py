ce184b41763e2baa277deb80127be3f77e31eac2
wendersoon <ds.wendersonmelo@gmail.com>
2025-08-15 07:53:48 -0300

    1.17

---
+++ chapter_1/exercise_1_17.py
@@ -0,0 +1,22 @@
+def mul(a, b):
+    if b == 0:
+        return 0
+    return a + mul(a, b - 1)
+
+print(mul(5,9))
+
+def double(a):
+    return a + a
+
+def halve(a):
+    return a / 2
+
+def fast_mul(a, b):
+    if b == 0:
+        return 0
+    elif b % 2 == 0:
+        return mul(double(a), halve(b))
+    else:
+        return a + mul(a, b - 1)
+    
+print(fast_mul(5, 10))
