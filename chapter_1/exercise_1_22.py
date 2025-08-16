7c08469bdc39e0a58eb7c32feb008b9702ff9609
wendersoon <ds.wendersonmelo@gmail.com>
2025-08-16 09:45:02 -0300

    1.22

---
+++ chapter_1/exercise_1_22.py
@@ -0,0 +1,44 @@
+import time
+
+def square(n):
+    return n * n
+
+def find_divisor(n , test_divisor):
+    if square(test_divisor) > n:
+        return n
+    elif n % test_divisor == 0:
+        return test_divisor
+    else:
+        return find_divisor(n, test_divisor + 1)
+
+def smallest_divisor(n):
+    return find_divisor(n, 2)
+
+def prime(n):
+    return n == smallest_divisor(n)
+
+def timed_prime_test(n):
+    print(n)
+    return start_prime_test(n, time.time())
+
+def start_prime_test(n, start_time):
+    if prime(n):
+        report_time(time.time() - start_time)
+        return True
+    print("nothing")
+    return False
+
+def report_time(elapsed_time):
+    print("***", elapsed_time)
+
+def search_for_primes(start_range, end_range):
+    if start_range % 2 == 0:   # garante que começa em ímpar
+        return search_for_primes(start_range + 1, end_range)
+    elif start_range > end_range:
+        print("done")
+        return
+    else:
+        timed_prime_test(start_range)
+        return search_for_primes(start_range + 2, end_range)
+
+search_for_primes(10000, 10009)
