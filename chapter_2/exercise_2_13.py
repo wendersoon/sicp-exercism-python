8043a449145faf2c020516c5b7bdacd2081103c7
wendersoon <ds.wendersonmelo@gmail.com>
2025-09-06 16:04:54 -0300

    2.13

--- chapter_2/exercise_2_13.py
+++ chapter_2/exercise_2_13.py
@@ -1,6 +1,6 @@
 """
 When the percentage tolerances are small, the tolerance of the product of two intervals is approximately the sum of the factor tolerances.
 
-function1 and function2 produce different results because interval arithmetic loses the dependencies between variables when they appear more than once, causing one method to generate wider intervals than the other.
+par1 and par2 produce different results because interval arithmetic loses the dependencies between variables when they appear more than once, causing one method to generate wider intervals than the other.
 
 """
